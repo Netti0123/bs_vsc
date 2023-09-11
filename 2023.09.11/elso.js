cdedd4286e4d8398e20932d51a8c93d64255fc1d
Netti0123 <anettdanko9852@gmail.com>
2023-09-11 09:03:19 +0200

    js_gyakorlas

---
+++ 2023.09.11/elso.js
@@ -0,0 +1,24 @@
+const a_input=document.getElementById("a_szam");
+const b_input=document.querySelector("#b_szam");
+const kijelzo=document.querySelector("#eredmeny");
+
+function osszead(){
+    a= parseFloat(a_input.value);
+    b= parseFloat(b_input.value);
+    kijelzo.innerHTML= "Összeadva: " + (a+b).toString();
+}
+function kivonas(){
+    a= parseFloat(a_input.value);
+    b= parseFloat(b_input.value);
+    kijelzo.innerHTML= "Kivonva: " + (a-b).toString();
+}
+function szorzas(){
+    a= parseFloat(a_input.value);
+    b= parseFloat(b_input.value);
+    kijelzo.innerHTML= "Kivonva: " + (a*b).toString();
+}
+function osztas(){
+    a= parseFloat(a_input.value);
+    b= parseFloat(b_input.value);
+    kijelzo.innerHTML= "Kivonva: " + (a/b).toString();
+}
